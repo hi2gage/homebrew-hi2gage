fbcb4705ec37a2eb3c04ed9e18d3b271c3f94ba4
Gage <gage@draftroomstudios.com>
2026-01-11 00:35:41 -0700

    Update BuildSignal to v0.1.0

--- Casks/buildsignal.rb
+++ Casks/buildsignal.rb
@@ -1,8 +1,8 @@
 cask "buildsignal" do
-  version "1.0.0"
+  version "0.1.0"
   sha256 "816f65c5acf3cf6509acae1b71020dab9e26b884d517f99747e61e35ed32c419"
 
-  url "https://github.com/hi2gage/BuildSignal/releases/download/v#{version}/BuildSignal-1.0.zip"
+  url "https://github.com/hi2gage/BuildSignal/releases/download/v#{version}/BuildSignal-#{version}.zip"
   name "BuildSignal"
   desc "Analyze Xcode build logs and warnings"
   homepage "https://github.com/hi2gage/BuildSignal"
